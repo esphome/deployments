e66dc37ec53c98a776207a2e599d7dcdd6442a71
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-10-12 20:09:36 +0200

    Add base infrastructure configuration (#1)

---
+++ .modules/service/module.tf
@@ -0,0 +1,4 @@
+data "tfe_outputs" "infrastructure" {
+  organization = "esphome"
+  workspace    = "infrastructure"
+}
---
+++ .modules/webservice/module.tf
@@ -0,0 +1,28 @@
+data "tfe_outputs" "infrastructure" {
+  organization = "esphome"
+  workspace    = "infrastructure"
+}
+
+module "webservice" {
+  source = "../service"
+
+  service_name           = var.service_name
+  container_image        = var.container_image
+  container_version      = var.container_version
+  launch_type            = var.launch_type
+  rolling_updates        = var.rolling_updates
+  region                 = var.region
+  ecs_cpu                = var.ecs_cpu
+  ecs_memory             = var.ecs_memory
+  webservice             = true
+  task_policy_statements = var.task_policy_statements
+
+  container_definitions = merge({
+    portMappings = [
+      {
+        containerPort = var.port
+        hostPort      = var.port
+      }
+    ],
+  }, var.container_definitions)
+}
