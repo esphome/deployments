25af9a195969648cc469f971cecebef459465a44
ludeeus <ludeeus@ludeeus.dev>
2022-10-13 17:33:16 +0000

    Add deployment for service_hub_bots

---
+++ service_hub_bots/variables.tf
@@ -0,0 +1,31 @@
+variable "discord_token" {
+  type = string
+}
+
+variable "discord_guild_id" {
+  type = string
+}
+
+variable "github_webhook_secret" {
+  type = string
+}
+
+variable "github_app_id" {
+  type = string
+}
+
+variable "github_installation_id" {
+  type = string
+}
+
+variable "github_key_contents" {
+  type = string
+}
+
+variable "sentry_dsn" {
+  type = string
+}
+
+variable "service_hub_image_tag" {
+  type = string
+}
