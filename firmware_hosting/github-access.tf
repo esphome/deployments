1574d640981fab0c813600ed2b3600503c411463
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-08-02 13:43:46 +1200

    Alter the secrets created in GH (#19)

--- firmware_hosting/github-access.tf
+++ firmware_hosting/github-access.tf
@@ -13,10 +13,40 @@ resource "cloudflare_api_token" "github-token" {
   }
 }
 
-resource "github_actions_organization_secret" "cloudflare-r2-upload-token" {
-  secret_name     = "CLOUDFLARE_R2_UPLOAD_TOKEN"
+resource "github_actions_organization_secret" "cloudflare-r2-account-id" {
+  secret_name     = "CLOUDFLARE_R2_ACCOUNT_ID"
   visibility      = "selected"
-  plaintext_value = cloudflare_api_token.github-token.value
+  plaintext_value = var.cloudflare_account_id
+
+  lifecycle {
+    ignore_changes = [selected_repository_ids]
+  }
+}
+
+resource "github_actions_organization_secret" "cloudflare-r2-bucket" {
+  secret_name     = "CLOUDFLARE_R2_BUCKET"
+  visibility      = "selected"
+  plaintext_value = var.bucket_name
+
+  lifecycle {
+    ignore_changes = [selected_repository_ids]
+  }
+}
+
+resource "github_actions_organization_secret" "cloudflare-r2-access-key-id" {
+  secret_name     = "CLOUDFLARE_R2_ACCESS_KEY_ID"
+  visibility      = "selected"
+  plaintext_value = cloudflare_api_token.github-token.id
+
+  lifecycle {
+    ignore_changes = [selected_repository_ids]
+  }
+}
+
+resource "github_actions_organization_secret" "cloudflare-r2-secret-access-key" {
+  secret_name     = "CLOUDFLARE_R2_SECRET_ACCESS_KEY"
+  visibility      = "selected"
+  plaintext_value = sha256(cloudflare_api_token.github-token.value)
 
   lifecycle {
     ignore_changes = [selected_repository_ids]
